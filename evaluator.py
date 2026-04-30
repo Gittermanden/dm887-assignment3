848ff42589a6aa7a94a37c82ca49fddc9571239f
Gittermanden <jeppssonalex@gmail.com>
2026-04-30 13:20:31 +0200

    user you own branch if you don't wanna use my code :p

--- evaluator.py
+++ evaluator.py
@@ -6,7 +6,7 @@ from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import VecFrameStack, SubprocVecEnv, VecTransposeImage
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
-from gymnasium.wrappers import FlattenObservation, GrayscaleObservation, ResizeObservation
+from gymnasium.wrappers import FlattenObservation, GrayscaleObservation, ResizeObservation, RescaleAction
 from gymnasium.spaces import Box
 
 
@@ -22,12 +22,15 @@ class ForceRGBChannel(gym.ObservationWrapper):
 
 def _make_car_env(render_mode=None):
     env = gym.make("CarRacing-v3", continuous=True, render_mode=render_mode)
-    env = GrayscaleObservation(env, keep_dim=True)
+    #env = gym.wrappers.MaxAndSkipObservation(env, skip=4)
     env = ResizeObservation(env, (64, 64))
-    env = ForceRGBChannel(env) 
+    env = GrayscaleObservation(env, keep_dim=True)
+
+    #env = gym.wrappers.ReshapeObservation(env, (64, 64, 1))
+    #env = ForceRGBChannel(env) 
     return env
 
-def run_experiment(env_id, algo_id, total_steps=100000, seed=42, n_envs=1, eval_freq=20000, buffer_size=100000):
+def run_experiment(env_id, algo_id, total_steps=100000, seed=42, n_envs=1, eval_freq=20000, buffer_size=500000):
     log_path = f"./eval_results/seed_{seed}/{env_id}/{algo_id}".replace("dm_control/", "")
     os.makedirs(log_path, exist_ok=True)
     print(f"--- Starting Experiment: {env_id}, using Model: {algo_id} ---")
@@ -40,12 +43,18 @@ def run_experiment(env_id, algo_id, total_steps=100000, seed=42, n_envs=1, eval_
 
     elif "CarRacing" in env_id:
         policy_type = "CnnPolicy"
-        env = make_vec_env(_make_car_env, n_envs=n_envs, seed=seed, vec_env_cls=SubprocVecEnv)
-        eval_env = make_vec_env(_make_car_env, n_envs=1, seed=seed)
+        env = make_vec_env(lambda:_make_car_env(), n_envs=n_envs, seed=seed, vec_env_cls=SubprocVecEnv)
+        eval_env = make_vec_env(lambda: _make_car_env(), n_envs=1, seed=seed)
+        
+        #env = make_vec_env(env_id, n_envs=n_envs, seed=seed, vec_env_cls=SubprocVecEnv, env_kwargs={"continuous": True})
+        #eval_env = make_vec_env(env_id, n_envs=1, seed=seed, env_kwargs={"continuous": True})
         
         # Stacking and Transposing
-        env = VecFrameStack(env, n_stack=4, channels_order="last")
-        eval_env = VecFrameStack(eval_env, n_stack=4, channels_order="last")
+        #env = VecFrameStack(env, n_stack=4, channels_order="last")
+        #eval_env = VecFrameStack(eval_env, n_stack=4, channels_order="last")
+        
+        env = VecFrameStack(env, n_stack=4)
+        eval_env = VecFrameStack(eval_env, n_stack=4)
         
         env = VecTransposeImage(env) # Changes [64, 64, 4] to [4, 64, 64]
         eval_env = VecTransposeImage(eval_env)
@@ -59,18 +68,20 @@ def run_experiment(env_id, algo_id, total_steps=100000, seed=42, n_envs=1, eval_
             log_path=log_path, 
             eval_freq=adjusted_eval_freq, 
             n_eval_episodes=10,
-            deterministic=True
+            deterministic=True,
+            # render=True # Uncomment to watch the evaluation unfold for testing purposes
         )
     tb_log_dir = f"./tb_logs/{seed}/{env_id}/{algo_id}/".replace("dm_control/", "")
     # 4. Initialize & Train
     if algo_id == "PPO":
-        model = PPO(policy_type, env, tensorboard_log=tb_log_dir, ent_coef=0.0075, seed=seed)
+        #model = PPO(policy_type, env, tensorboard_log=tb_log_dir, ent_coef=0.0075, seed=seed)
+        model = PPO(policy_type, env, tensorboard_log=tb_log_dir, seed=seed, batch_size=256)
     elif algo_id == "SAC":
-        model = SAC(policy_type, env, tensorboard_log=tb_log_dir, buffer_size=buffer_size, seed=seed)
+        model = SAC(policy_type, env, tensorboard_log=tb_log_dir, buffer_size=buffer_size, seed=seed, learning_rate=1e-4)
     elif algo_id == "TD3":
         n_actions = env.action_space.shape[-1]
-        action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
-        model = TD3(policy_type, env, tensorboard_log=tb_log_dir, buffer_size=buffer_size, action_noise=action_noise, seed=seed)
+        action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.3 * np.ones(n_actions)) # sigma = 0.1 leads to too little exploration.
+        model = TD3(policy_type, env, tensorboard_log=tb_log_dir, buffer_size=buffer_size, action_noise=action_noise, seed=seed, learning_rate=1e-4)
         
     model.learn(total_timesteps=total_steps, callback=eval_callback, progress_bar=True)
     env.close()
--- main.py
+++ main.py
@@ -8,36 +8,37 @@ from shimmy import dm_control_compatibility
 
 
 if __name__ == "__main__":
-    """
+    
     environments = [
         #{"id":"dm_control/cartpole-swingup-v0", "steps": 100000, "eval_freq": 10000}, 
         #{"id": "dm_control/acrobot-swingup-v0", "steps": 200000, "eval_freq": 10000}, 
-        #{"id":"CarRacing-v3", "steps": 1000000,"eval_freq": 50000}
+        {"id":"CarRacing-v3", "steps": 1000000,"eval_freq": 50000}
     ]
-    """
-        
+    
+    """    
     environments = [
         #{"id":"dm_control/cartpole-swingup-v0", "steps": 2000, "eval_freq": 200}, 
         #{"id": "dm_control/acrobot-swingup-v0", "steps": 2000, "eval_freq": 200}, 
         {"id":"CarRacing-v3", "steps": 500000,"eval_freq": 50000}
     ]
-    
+    """
     #algorithms = ["SAC", "TD3", "PPO"]
-    algorithms = ["TD3"]
+    algorithms = ["TD3", "SAC", "PPO"]
     #seed_pool = [5, 8, 32, 67, 154]
-    seed_pool = [56]
+    seed_pool = [67]
 
     for seed in seed_pool:
         for env in environments:
-            #results = []
+            results = []
             for algo in algorithms:
+                n_envs = 8 if algo == "PPO" else 1 # PPO is the only algorithm that is built around multiple environments...
                 try:
                     log_path = run_experiment(
                         env_id=env['id'], 
                         algo_id=algo,
                         total_steps=env["steps"],
                         seed=seed,
-                        n_envs=8, # PPO is the only algorithm that is built around multiple environments, but, whatever...
+                        n_envs=n_envs, 
                         eval_freq=env["eval_freq"],
                     )
 
@@ -50,13 +51,13 @@ if __name__ == "__main__":
                     # Plot the learning curve for the environment
                     try:
                         plot_results(log_path, env['id'], algo)
-                        #results.append({"algo": algo, "path": log_path}) # For comparison plot
+                        results.append({"algo": algo, "path": log_path}) # For comparison plot
                     except Exception as e:
                         print(f"Failed to graph {algo} on {env['id']}': {e}")
                 except Exception as e:
                     print(f"Failed to run {algo} on {env['id']}': {e}")
             # Compare the algorithms on the environment
-            #try:
-            #    compare_results(results, env['id'])
-            #except Exception as e:
-            #    print(f"Failed to compare algorithms on {env['id']}': {e}")
+            try:
+                compare_results(results, env['id'])
+            except Exception as e:
+               print(f"Failed to compare algorithms on {env['id']}': {e}")
